dcc09dc34ab7e28b3ca5e177ded90a87d90e945a
HaDH <gusdk123123@naver.com>
2023-01-13 17:37:11 +0900

    메인화면

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,31 @@
+const totalMenu = document.querySelector(".total-menu");
+const season = document.querySelector(".season");
+const coffee = document.querySelector(".coffee");
+const beverage = document.querySelector(".beverage");
+const tea = document.querySelector(".tea");
+const desert = document.querySelector(".desert");
+
+
+totalMenu.onclick = () => {
+
+}
+
+season.onclick = () => {
+
+}
+
+coffee.onclick = () => {
+
+}
+
+beverage.onclick = () => {
+
+}
+
+tea.onclick = () => {
+
+}
+
+desert.onclick = () => {
+
+}
