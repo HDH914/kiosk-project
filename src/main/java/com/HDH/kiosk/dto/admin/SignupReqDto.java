b8c1f60cb6a764c0bc0c806dd010b4c9496a1869
HaDH <gusdk123123@naver.com>
2023-01-17 17:37:26 +0900

    DTO

---
+++ src/main/java/com/HDH/kiosk/dto/admin/SignupReqDto.java
@@ -0,0 +1,20 @@
+package com.HDH.kiosk.dto.admin;
+
+import com.HDH.kiosk.domain.User;
+import lombok.Data;
+
+@Data
+public class SignupReqDto {
+
+    private String adminId;
+    private String password;
+    private int storeNumber;
+
+    public User toUserEntity() {
+        return User.builder()
+                .admin_id(adminId)
+                .password(password)
+                .store_number(storeNumber)
+                .build();
+    }
+}
