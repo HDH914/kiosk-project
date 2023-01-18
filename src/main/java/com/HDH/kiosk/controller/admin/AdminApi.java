7c2654418e7bb7135e73dddda9bff17173825cf8
HaDH <gusdk123123@naver.com>
2023-01-18 19:22:54 +0900

    관리자 RestController

--- src/main/java/com/HDH/kiosk/controller/admin/AdminApi.java
+++ src/main/java/com/HDH/kiosk/controller/admin/AdminApi.java
@@ -1,12 +1,14 @@
 package com.HDH.kiosk.controller.admin;
 
 import com.HDH.kiosk.dto.CMRespDto;
+import com.HDH.kiosk.dto.TestDto;
 import com.HDH.kiosk.dto.admin.SignupReqDto;
 import com.HDH.kiosk.service.admin.AdminService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdminApi {
 
-    private final AdminService adminService;
+    private AdminService adminService;
 
     // 회원 가입
     @PostMapping("/signup")
     public ResponseEntity<?> signup(SignupReqDto signupReqDto)throws Exception {
         adminService.signup(signupReqDto);
-        return ResponseEntity.ok().body(new CMRespDto<>(1, "회원 가입", signupReqDto));
+        return ResponseEntity.ok().body(new CMRespDto<>(1, "회원가입 완료", signupReqDto));
+    }
+
+
+
+    //////////////////////////
+    @PostMapping("/testajax")
+    public ResponseEntity<?> testajax(TestDto testDto) throws Exception {
+        adminService.testAjax(testDto);
+        return ResponseEntity.ok().body(new CMRespDto<>(1, "testAjax",testDto));
     }
 }
