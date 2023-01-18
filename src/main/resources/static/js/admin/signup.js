e20ed00e3b2f46daa01682b5865deb04ddf90046
HaDH <gusdk123123@naver.com>
2023-01-18 19:21:56 +0900

    회원가입

--- src/main/resources/static/js/admin/signup.js
+++ src/main/resources/static/js/admin/signup.js
@@ -1,17 +1,30 @@
 // -Validation
 
-
 const signupInput = document.querySelectorAll(".signup-input");    // 회원가입 정보
 const signupButton = document.querySelector(".signup-button");    // 회원가입 버튼
+
+for (let i = 0; i < signupInput.length; i++) {
+    signupInput[i].onkeyup = () => {
+        if (window.event.keyCode === 13) {
+            if (i != 4) {
+                signupInput[i + 1].focus();
+            } else {
+                signupButton.click();
+            }
+        }
+    }
+}
 
 signupButton.onclick = () => {
     // 회원가입시 인풋 정보
     let signupInfo = {
         userid: signupInput[0].value,
         password: signupInput[1].value,
-        passwordCheck: signupInput[2].value,
-        adminNumber: signupInput[3].value
+        adminNumber: signupInput[2].value
     }
+
+    console.log(signupInfo);
+
 
     $.ajax({
         async: false,
@@ -21,13 +34,13 @@ signupButton.onclick = () => {
         data: JSON.stringify(signupInfo),
         dataType: "json",
         success: (response) => {
+            alert("회원가입 완료.");
+            location.replace("/");
             console.log(response.data);
-            alert("회원가입 완료!");
-            location.replace("/login");
         },
         error: (error) => {
             console.log(error);
+            console.log("전송 실패");
         }
-
-    })
+    });
 }
