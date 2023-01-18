772c34dadeda84e8d79f44d9318fd225cf6f4257
HaDH <gusdk123123@naver.com>
2023-01-18 19:22:32 +0900

    서비스 레이어

--- src/main/java/com/HDH/kiosk/service/admin/AdminServiceImpl.java
+++ src/main/java/com/HDH/kiosk/service/admin/AdminServiceImpl.java
@@ -1,22 +1,29 @@
 package com.HDH.kiosk.service.admin;
 
+import com.HDH.kiosk.dto.TestDto;
 import com.HDH.kiosk.dto.admin.SignupReqDto;
-import com.HDH.kiosk.repository.admin.AdminRepository;
+import com.HDH.kiosk.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class AdminServiceImpl implements AdminService {
+public class AdminServiceImpl implements AdminService{
+
     private final AdminRepository adminRepository;
 
     // 회원가입
     @Override
-    public boolean signup(SignupReqDto signupReqDto) throws Exception {
+    public boolean signup(SignupReqDto signupReqDto) throws Exception{
+
         return adminRepository.save(signupReqDto.toUserEntity()) != 0;
+    }
+
+    @Override
+    public boolean testAjax(TestDto testDto) throws Exception {
+        return adminRepository.testAjax(testDto.testAjax()) != 0;
     }
 
 
