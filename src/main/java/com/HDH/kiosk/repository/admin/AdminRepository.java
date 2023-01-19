91e8a4e1c4a028e186311c7d950da16ab49227fe
HaDH <gusdk123123@naver.com>
2023-01-19 21:59:27 +0900

    Admin 레파지토리 레이어

--- src/main/java/com/HDH/kiosk/repository/admin/AdminRepository.java
+++ src/main/java/com/HDH/kiosk/repository/admin/AdminRepository.java
@@ -1,9 +1,10 @@
 package com.HDH.kiosk.repository.admin;
 
-import com.HDH.kiosk.domain.User;
+import com.HDH.kiosk.domain.User2;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface AdminRepository {
-    public int save(User user);
+    public int save(User2 user2);
+
 }
