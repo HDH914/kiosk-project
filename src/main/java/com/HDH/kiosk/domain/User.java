18684ad057bb8259035a691353819c504467ac02
HaDH <gusdk123123@naver.com>
2023-01-24 18:18:12 +0900

    User Domain

--- src/main/java/com/HDH/kiosk/domain/User.java
+++ src/main/java/com/HDH/kiosk/domain/User.java
@@ -15,7 +15,7 @@ public class User {
     private int id;
     private String admin_id;
     private String password;
-    private int store_number;
+    private String store_number;
     private LocalDateTime create_date;
     private LocalDateTime update_date;
 }
