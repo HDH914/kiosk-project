d42ea163dda67ab11a7f92f0900f55eda2b8ca5a
HaDH <gusdk123123@naver.com>
2023-01-27 19:48:15 +0900

    로그인

--- src/main/resources/static/js/admin/login.js
+++ src/main/resources/static/js/admin/login.js
@@ -1,7 +1,21 @@
-const loginForm = document.querySelector(".login-form");
+
+const loginInput = document.querySelectorAll(".general-login-input");
 const loginButton = document.querySelector(".login-button");
 
+for (let i = 0; i < loginInput.length; i++) {
+    loginInput[i].onkeyup = () => {
+        if (window.event.keyCode === 13) {
+            if (i != 4) {
+                loginInput[i + 1].focus();
+            } else {
+                loginButton.click();
+            }
+        }
+    }
+}
+
 loginButton.onclick = () => {
-    loginForm.subnit();
+    const loginForm = document.querySelector("form");
+    loginForm.submit();
     console.log("폼 전송 완료");
 }
