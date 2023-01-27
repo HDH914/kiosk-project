7e25229c722077f57860e5ecbc309fd4c22499d5
HaDH <gusdk123123@naver.com>
2023-01-27 19:49:25 +0900

    회원가입 Dto

--- src/main/java/com/HDH/kiosk/dto/admin/SignupReqDto.java
+++ src/main/java/com/HDH/kiosk/dto/admin/SignupReqDto.java
@@ -2,6 +2,7 @@ package com.HDH.kiosk.dto.admin;
 
 import com.HDH.kiosk.domain.User;
 import lombok.Data;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -9,17 +10,19 @@ import javax.validation.constraints.Pattern;
 @Data
 public class SignupReqDto {
     @NotBlank(message = "아이디를 입력해주세요.")
-    private String adminId;
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]{8,16}$",
-            message = "숫자, 영문, 특수기호를 하나 이상 포함하여 8자 이상 16자 이하로 작성해야합니다.")
+    private String username;
+
     private String password;
     @NotBlank
     private String storeNumber;
 
+    //    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]{8,16}$",
+//            message = "숫자, 영문, 특수기호를 하나 이상 포함하여 8자 이상 16자 이하로 작성해야합니다.")
+
     public User toUserEntity() {
         return User.builder()
-                .admin_id(adminId)
-                .password(password)
+                .username(username)
+                .password(new BCryptPasswordEncoder().encode(password))
                 .store_number(storeNumber)
                 .build();
     }
