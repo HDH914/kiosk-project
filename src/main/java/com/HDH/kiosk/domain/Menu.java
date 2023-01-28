69748f6704df5302ea464600fadc583ca3d3e9da
HaDH <gusdk123123@naver.com>
2023-01-28 20:33:54 +0900

    메뉴 도메인

---
+++ src/main/java/com/HDH/kiosk/domain/Menu.java
@@ -0,0 +1,25 @@
+package com.HDH.kiosk.domain;
+
+import com.HDH.kiosk.dto.menu.AddMenuReqDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Menu {
+    private int id;
+    private int category_id;
+    private String menu_name;
+    private int price;
+    private String menu_img;
+    private LocalDateTime create_date;
+    private LocalDateTime update_date;
+
+
+}
