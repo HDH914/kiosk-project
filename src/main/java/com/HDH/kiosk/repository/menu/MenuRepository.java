3db5267d3eb06fbaef7bee6f5f37fac920d59552
HaDH <gusdk123123@naver.com>
2023-01-28 20:33:44 +0900

    메뉴 레파지토리 레이어

---
+++ src/main/java/com/HDH/kiosk/repository/menu/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.HDH.kiosk.repository.menu;
+
+import com.HDH.kiosk.domain.Menu;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MenuRepository {
+    public int addMenu(Menu menu);
+}
