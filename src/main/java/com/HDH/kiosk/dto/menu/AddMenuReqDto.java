1569ee58a98f12de55f1170cef89075430421bb8
HaDH <gusdk123123@naver.com>
2023-01-28 20:33:32 +0900

    메뉴 추가 Dto

---
+++ src/main/java/com/HDH/kiosk/dto/menu/AddMenuReqDto.java
@@ -0,0 +1,29 @@
+package com.HDH.kiosk.dto.menu;
+
+import com.HDH.kiosk.domain.Menu;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class AddMenuReqDto {
+    private int id;
+    private int categoryId;
+    private String menuName;
+    private int price;
+    private String menuImg;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    public Menu toAddMenu(){
+        return Menu.builder()
+                .id(id)
+                .category_id(categoryId)
+                .menu_name(menuName)
+                .price(price)
+                .menu_img(menuImg)
+                .create_date(createDate)
+                .update_date(updateDate)
+                .build();
+    }
+}
