2599b04252b1ed3f47bc297757950d9e6d867ce2
HaDH <gusdk123123@naver.com>
2023-01-29 19:33:58 +0900

    메뉴 서비스 레이어

--- src/main/java/com/HDH/kiosk/service/menu/MenuService.java
+++ src/main/java/com/HDH/kiosk/service/menu/MenuService.java
@@ -1,7 +1,10 @@
 package com.HDH.kiosk.service.menu;
 
 import com.HDH.kiosk.dto.menu.AddMenuReqDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MenuService {
     public void addMenu(AddMenuReqDto addMenuReqDto) throws Exception;
+
+    public void addImage(AddMenuReqDto addMenuReqDto, MultipartFile file) throws Exception;
 }
--- src/main/java/com/HDH/kiosk/service/menu/MenuServiceImpl.java
+++ src/main/java/com/HDH/kiosk/service/menu/MenuServiceImpl.java
@@ -4,7 +4,16 @@ import com.HDH.kiosk.dto.menu.AddMenuReqDto;
 import com.HDH.kiosk.repository.menu.MenuRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -12,9 +21,44 @@ import org.springframework.stereotype.Service;
 public class MenuServiceImpl implements MenuService{
     private final MenuRepository menuRepository;
 
+    @Value("${file.path")
+    private String filePath;
+
 
     @Override
     public void addMenu(AddMenuReqDto addMenuReqDto) throws Exception{
+
+
+
+        int menuId = addMenuReqDto.getId();
+        log.info("메뉴ID: " + menuId);
+
+
+
         menuRepository.addMenu(addMenuReqDto.toAddMenu());
+    }
+
+    @Override
+    public void addImage(AddMenuReqDto addMenuReqDto,MultipartFile file) throws Exception {
+        String originName = file.getOriginalFilename();
+        String extension = originName.substring(originName.lastIndexOf("."));
+        String tempName = UUID.randomUUID().toString() + extension;
+
+        Path uploadPath = Paths.get(filePath + "/menu/" + tempName);
+        addMenuReqDto.setMenuImg(tempName);
+        log.info("파일 이름: " + tempName);
+
+        // 파일 경로
+        File f = new File(filePath + "/menu");
+
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+        try{
+            Files.write(uploadPath, file.getBytes());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
