99835524ba6db32df7e4d6689a09ac1f6fe4ad32
HaDH <gusdk123123@naver.com>
2023-02-02 20:59:49 +0900

    Revert "Revert "어드민 페이지 수정 및 메뉴 데이터 불러오기""
    This reverts commit 48e7aa01dc37ed4ff5713abb860bb264022d73e0.

--- src/main/java/com/HDH/kiosk/repository/menu/MenuRepository.java
+++ src/main/java/com/HDH/kiosk/repository/menu/MenuRepository.java
@@ -5,12 +5,11 @@ import com.HDH.kiosk.domain.MenuList;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-import java.util.Map;
 
 @Mapper
 public interface MenuRepository {
     public int addMenu(Menu menu);
 
-    public List<MenuList> loadMenuList(Map<String, Object> map)  throws Exception;
+    public List<MenuList> loadMenuList(String searchValue) throws Exception;
 
 }
