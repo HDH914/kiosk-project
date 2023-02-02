9329ff5d65e1185c2d802cd875014841db829f0c
HaDH <gusdk123123@naver.com>
2023-02-02 21:01:26 +0900

    메뉴 불러오기 도메인

---
+++ src/main/java/com/HDH/kiosk/domain/MenuList.java
@@ -0,0 +1,31 @@
+package com.HDH.kiosk.domain;
+
+import com.HDH.kiosk.dto.menu.MenuListRespDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuList {
+    private int id;
+    private int category_id;
+    private String menu_name;
+    private int price;
+    private String menu_img;
+    private String memo;
+
+    public MenuListRespDto toLoadMenu(){
+        return MenuListRespDto.builder()
+                .id(id)
+                .categoryId(category_id)
+                .menuName(menu_name)
+                .price(price)
+                .menuImg(menu_img)
+                .memo(memo)
+                .build();
+    }
+}
