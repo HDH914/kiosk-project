7bbabd2860eee4e0c20063b5ca2d0cb79267dfb3
HaDH <gusdk123123@naver.com>
2023-02-03 14:47:47 +0900

    메뉴 리스트 도메인

--- src/main/java/com/HDH/kiosk/domain/MenuList.java
+++ src/main/java/com/HDH/kiosk/domain/MenuList.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class MenuList {
     private int id;
     private int category_id;
+    private String category_name;
     private String menu_name;
     private int price;
     private String menu_img;
@@ -22,6 +23,7 @@ public class MenuList {
         return MenuListRespDto.builder()
                 .id(id)
                 .categoryId(category_id)
+                .categoryName(category_name)
                 .menuName(menu_name)
                 .price(price)
                 .menuImg(menu_img)
