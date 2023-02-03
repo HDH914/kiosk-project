b9a8dbd5fb44119e0512b5d0e56fadaeb7555f3f
HaDH <gusdk123123@naver.com>
2023-02-03 14:48:27 +0900

    어드민 페이지

--- src/main/resources/static/js/admin/admin.js
+++ src/main/resources/static/js/admin/admin.js
@@ -3,6 +3,8 @@ const searchInput = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-button");
 let searchValue = "";
 
+loadMenuListRequest(searchValue);
+
 searchInput.onkeyup = () => {
     if (window.event.keyCode === 13) {
         searchButton.click();
@@ -10,14 +12,11 @@ searchInput.onkeyup = () => {
 }
 
 searchButton.onclick = () => {
-    alert("클릭됨");
     searchValue = searchInput.value;
     loadMenuListRequest(searchValue);
 }
 
-
-
-function loadMenuListRequest() {
+function loadMenuListRequest(searchValue) {
     let responseData = null;
     $.ajax({
         async: false,
@@ -29,9 +28,7 @@ function loadMenuListRequest() {
         dataType: "json",
         success: (response) => {
             responseData = response.data;
-            alert("불러오기 성공");
-            console.log("메뉴 데이터");
-            console.log(responseData);
+            loadMenuList(responseData);
         },
         error: (error) => {
             console.log(error);
@@ -40,3 +37,25 @@ function loadMenuListRequest() {
     })
 }
 
+function loadMenuList(responseData) {
+    const menuList = document.querySelector(".menu");
+
+    menuList.innerHTML = "";
+
+    responseData.forEach((data) => {
+        menuList.innerHTML += `
+            <tr class="menu-list">
+                <td class="id">${data.id}</td>
+                <td class="menu-image">${data.menuImg}</td>
+                <td class="menu-category">${data.categoryName}</td>
+                <td class="menu-name">${data.menuName}</td>
+                <td class="menu-price">${data.price}<span>원</span></td>
+                <td class="memo">${data.memo}</td>
+                <td class="menu-button buttons">
+                    <a class="modification-button" href="/admin/menuDetail/${data.id}">수정</a>
+                    <a class="delete-button" href="">삭제</a>
+                </td>
+            </tr>
+            `;
+    });
+}
