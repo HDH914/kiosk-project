cc74d585c7896c89c856e6d57a1b7f7615d5b641
HaDH <gusdk123123@naver.com>
2023-02-04 14:21:38 +0900

    메뉴 수정

--- src/main/resources/static/js/menu/modification.js
+++ src/main/resources/static/js/menu/modification.js
@@ -1,11 +1,12 @@
-const uri = location.href;
-const id = uri.substring(uri.lastIndexOf("/") + 1);
 const menuInfoArea = document.querySelector(".menu-info-area");
 const memoInfoArea = document.querySelector(".menu-memo-area");
+const saveButton = document.querySelector(".save-button");
+const cancelButton = document.querySelector(".cancel-button");
+const uri = location.href;
+const id = uri.substring(uri.lastIndexOf("/") + 1);
 
-
+// 메뉴 정보 불러오기
 loadMenuInfo(id);
-
 function loadMenuInfo(id) {
     let responseData = null;
 
@@ -24,7 +25,7 @@ function loadMenuInfo(id) {
         }
     });
 }
-
+// 불러온 메뉴정보 띄우기
 function menuInfo(responseData) {
     menuInfoArea.innerHTML = `
     <!-- 메뉴 이미지 -->
@@ -71,4 +72,65 @@ function menuInfo(responseData) {
         <textarea class="menu-memo menu-input" name="" id="" cols="30" rows="10" value="${responseData.memo}"></textarea>
     </div>
     `
+}
+// 수정된 메뉴 정보 저장
+saveButton.onclick = () => {
+    const menuInput = document.querySelectorAll(".menu-input");
+    let imageInput = document.querySelector(".image-input");
+    let formData = new FormData();
+
+    formData.append("menuImg", imageInput.value);
+    formData.append("menuName", menuInput[0].value);
+    formData.append("categoryId", menuInput[1].value);
+    formData.append("price", menuInput[2].value);
+    formData.append("memo", menuInput[3].value);
+
+    formData.forEach((value, key) => {
+        console.log(key);
+        console.log(value);
+        console.log();
+    });
+    if (menuInput[1].value == 0) {
+        alert("카테고리를 선택해주세요.");
+    } else {
+        updateMenu(formData);
+    }
+}
+
+// 메뉴 수정 데이터 보내기
+function updateMenu(formData) {
+    $.ajax({
+        async: false,
+        type: "put",
+        url: "/api/menu/admin/modification/update/" + id,
+        enctype: "multipart/form-data",
+        contentType: false,
+        processData: false,
+        data: formData,
+        dataType: "json",
+        success: (response) => {
+            alert("수정 성공");
+            let msg = null;
+            msg = confirm("페이지에서 나가시겠습니까?");
+            if (msg) {
+                location.href = "/admin";
+            } else {
+                location.reload();
+            }
+        },
+        error: (error) => {
+            console.log(error);
+            alert("수정 실패");
+        }
+    });
+}
+
+// 취소 버튼
+cancelButton.onclick = () => {
+    let msg = null;
+    msg = confirm("정말로 취소하시겠습니까?");
+
+    if (msg) {
+        location.href = "/admin";
+    }
 }
