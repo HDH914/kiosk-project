79e9044dfbc4981ecba66c8055af92e2c36d093b
HaDH <gusdk123123@naver.com>
2023-02-05 21:58:04 +0900

    Server Error Exceoption

---
+++ src/main/java/com/HDH/kiosk/exception/CustomInternalServerErrorException.java
@@ -0,0 +1,6 @@
+package com.HDH.kiosk.exception;
+
+public class CustomInternalServerErrorException extends RuntimeException {
+
+    public CustomInternalServerErrorException(String message) {super(message);}
+}
