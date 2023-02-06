ee80ed679b5dbe1765d4c762f61bd144233ba8ab
HaDH <gusdk123123@naver.com>
2023-02-06 23:24:06 +0900

    메뉴 레파지토리

--- src/main/java/com/HDH/kiosk/repository/menu/MenuRepository.java
+++ src/main/java/com/HDH/kiosk/repository/menu/MenuRepository.java
@@ -1,6 +1,7 @@
 package com.HDH.kiosk.repository.menu;
 
 import com.HDH.kiosk.domain.Menu;
+import com.HDH.kiosk.domain.MenuImage;
 import com.HDH.kiosk.domain.MenuList;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Mapper
 public interface MenuRepository {
     public int addMenu(Menu menu);
+    public int addMenuImg(MenuImage menuImage) throws Exception;
 
     public List<MenuList> loadMenuList(String searchValue) throws Exception;
 
