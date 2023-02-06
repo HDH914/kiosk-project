b09f69a71ea923e2ba28a4af5f4240c56b91c990
HaDH <gusdk123123@naver.com>
2023-02-06 23:22:40 +0900

    Menu 도메인

--- src/main/java/com/HDH/kiosk/domain/Menu.java
+++ src/main/java/com/HDH/kiosk/domain/Menu.java
@@ -1,10 +1,12 @@
 package com.HDH.kiosk.domain;
 
 import com.HDH.kiosk.dto.menu.AddMenuReqDto;
+import com.HDH.kiosk.dto.menu.MenuListRespDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -17,10 +19,21 @@ public class Menu {
     private int category_id;
     private String menu_name;
     private int price;
-    private String menu_img;
+    private MultipartFile menu_img;
     private String memo;
     private LocalDateTime create_date;
     private LocalDateTime update_date;
 
-
+//    public MenuListRespDto getMenuList(){
+//        return MenuListRespDto.builder()
+//                .id(id)
+//                .categoryId(category_id)
+//                .menuName(menu_name)
+//                .price(price)
+//                .menuImg(menu_img)
+//                .memo(memo)
+//                .build();
+//
+//
+//    }
 }
