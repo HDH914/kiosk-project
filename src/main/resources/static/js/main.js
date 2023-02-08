77a65871525a1c97dde0523d266e71085c5ee7a8
HaDH <gusdk123123@naver.com>
2023-02-08 20:45:45 +0900

    메인 페이지

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,16 @@
 const home = document.querySelector(".home");
-// const totalMenu = document.querySelector(".total-menu");
 const season = document.querySelector(".season");
 const coffee = document.querySelector(".coffee");
 const beverage = document.querySelector(".beverage");
 const tea = document.querySelector(".tea");
 const desert = document.querySelector(".desert");
-
-
-
+const menus = document.querySelector(".menus");
+const menu = document.querySelectorAll(".menu");
 
 
 loadMenuList();
 function loadMenuList() {
+    console.log("loadMenuList")
     let responseData = null;
     $.ajax({
         async: false,
@@ -20,74 +19,141 @@ function loadMenuList() {
         // dataType: "jspn",
         success: (response) => {
             responseData = response.data;
-            console.log("불러오기 성공")
-            console.log(responseData)
-            loadMenu(responseData);
+            console.log(responseData);
+            clickSeason(responseData);
+            clickoffee(responseData);
+            clickBeverage(responseData);
+            clickTea(responseData);
+            clickDesert(responseData);
+            season.click();
         },
         error: (error) => {
-            console.log("불러오기 실패")
+            console.log("불러오기 실패");
             console.log(error)
         }
     })
 }
 
-function loadMenu(responseData) {
-    const menus = document.querySelector(".menus");
-    let categoryId = ${ data.categoryId };
 
-    menus.innerHTML = "";
-    responseData.forEach((data) => {
-        menus.innerHTML += `
-        <li class="menu">
-            <input class="menu-id" type="text" value="${data.id}">
-            <input class="menu-category" type="text" value="${data.categoryId}">
-            <div>
-                <img src="/image/menu/${data.menuImg.temp_name}" alt="">
-            </div>
-            <div class="menu-name">${data.menuName}</div>
-            <div class="menu-price">${data.price}<span>원</span></div>
-        </li>
-        `
-    });
 
-    if ()
-}
-
+// 홈버튼
 home.onclick = () => {
     location.href = "/";
 }
 
-// totalMenu.onclick = () => {
+function clickSeason(responseData) {
+    season.onclick = () => {
+        menus.innerHTML = "";
+        responseData.forEach((data) => {
+            if (data.categoryId == 1) {
+                menus.innerHTML += `
+                <li class="menu">
+                    <input class="menu-id" type="text" value="${data.id}">
+                    <input class="menu-category" type="text" value="${data.categoryId}">
+                    <div>
+                        <img src="/image/menu/${data.menuImg.temp_name}" alt="">
+                    </div>
+                    <div class="menu-name">${data.menuName}</div>
+                    <div class="menu-price">${data.price}<span>원</span></div>
+                </li>
+                `
+            }
+        });
+    }
+}
 
+function clickoffee(responseData) {
+    coffee.onclick = () => {
+        menus.innerHTML = "";
+        responseData.forEach((data) => {
+            if (data.categoryId == 2) {
+                menus.innerHTML += `
+                <li class="menu">
+                    <input class="menu-id" type="text" value="${data.id}">
+                    <input class="menu-category" type="text" value="${data.categoryId}">
+                    <div>
+                        <img src="/image/menu/${data.menuImg.temp_name}" alt="">
+                    </div>
+                    <div class="menu-name">${data.menuName}</div>
+                    <div class="menu-price">${data.price}<span>원</span></div>
+                </li>
+                `
+            }
+        });
+    }
+}
+
+function clickBeverage(responseData) {
+    beverage.onclick = () => {
+        menus.innerHTML = "";
+        responseData.forEach((data) => {
+            if (data.categoryId == 3) {
+                menus.innerHTML += `
+                <li class="menu">
+                    <input class="menu-id" type="text" value="${data.id}">
+                    <input class="menu-category" type="text" value="${data.categoryId}">
+                    <div>
+                        <img src="/image/menu/${data.menuImg.temp_name}" alt="">
+                    </div>
+                    <div class="menu-name">${data.menuName}</div>
+                    <div class="menu-price">${data.price}<span>원</span></div>
+                </li>
+                `
+            }
+        });
+    }
+}
+
+function clickTea(responseData) {
+    tea.onclick = () => {
+        menus.innerHTML = "";
+        responseData.forEach((data) => {
+            if (data.categoryId == 4) {
+                menus.innerHTML += `
+                <li class="menu">
+                    <input class="menu-id" type="text" value="${data.id}">
+                    <input class="menu-category" type="text" value="${data.categoryId}">
+                    <div>
+                        <img src="/image/menu/${data.menuImg.temp_name}" alt="">
+                    </div>
+                    <div class="menu-name">${data.menuName}</div>
+                    <div class="menu-price">${data.price}<span>원</span></div>
+                </li>
+                `
+            }
+        });
+    }
+}
+
+function clickDesert(responseData) {
+    desert.onclick = () => {
+        menus.innerHTML = "";
+        responseData.forEach((data) => {
+            if (data.categoryId == 5) {
+                menus.innerHTML += `
+                <li class="menu">
+                    <input class="menu-id" type="text" value="${data.id}">
+                    <input class="menu-category" type="text" value="${data.categoryId}">
+                    <div>
+                        <img src="/image/menu/${data.menuImg.temp_name}" alt="">
+                    </div>
+                    <div class="menu-name">${data.menuName}</div>
+                    <div class="menu-price">${data.price}<span>원</span></div>
+                </li>
+                `
+            }
+        });
+    }
+}
+
+// menu.onclick = () => {
+//     // 클릭을 하면 클릭한 메뉴를 리스트에 담고 그 리스트 정보들을
+//     // 뿌린다.
+//     const menuList = document.querySelector(".menu-list");
+
+//     menuList.innerHTML = `
+    
+//     `
 // }
 
-function clickSeason() {
-    season.onclick = () => {
-
-    }
-}
-
-function clickoffee() {
-    coffee.onclick = () => {
-
-    }
-}
-
-function clickBeverage() {
-    beverage.onclick = () => {
-
-    }
-}
-
-function clickTea() {
-    tea.onclick = () => {
-
-    }
-}
-
-function clickDesert() {
-    desert.onclick = () => {
-
-    }
-}
 
