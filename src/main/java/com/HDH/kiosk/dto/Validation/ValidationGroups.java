b1bd9f3f3b00063f8b5695255b88e8d3a74d5083
HaDH <gusdk123123@naver.com>
2023-02-10 19:22:16 +0900

    Validation Groups

---
+++ src/main/java/com/HDH/kiosk/dto/Validation/ValidationGroups.java
@@ -0,0 +1,7 @@
+package com.HDH.kiosk.dto.Validation;
+
+public interface ValidationGroups {
+    public interface NotBlackGroup{};
+    public interface SizeCheckGroups{};
+    public interface PatternCheckGroup{};
+}
