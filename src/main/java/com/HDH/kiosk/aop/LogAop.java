7e2a0c031126105ff129fdc3d00c9c808844c032
HaDH <gusdk123123@naver.com>
2023-02-11 18:51:07 +0900

    Log Aop

---
+++ src/main/java/com/HDH/kiosk/aop/LogAop.java
@@ -0,0 +1,11 @@
+package com.HDH.kiosk.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAop {
+}
