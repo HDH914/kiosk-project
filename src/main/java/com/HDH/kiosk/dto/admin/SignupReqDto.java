255e1ae4c389a5fbbcc3a8f0fff2a8898ee7038b
HaDH <gusdk123123@naver.com>
2023-02-15 19:49:32 +0900

    회원가입 DTO

--- src/main/java/com/HDH/kiosk/dto/admin/SignupReqDto.java
+++ src/main/java/com/HDH/kiosk/dto/admin/SignupReqDto.java
@@ -11,9 +11,10 @@ import javax.validation.constraints.Pattern;
 public class SignupReqDto {
     @NotBlank(message = "아이디를 입력해주세요.")
     private String username;
-
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]{8,16}$",
+            message = "숫자, 영문, 특수기호를 하나 이상 포함하여 8자 이상 16자 이하로 작성해야합니다.")
     private String password;
-    @NotBlank
+    @NotBlank(message = "사업장 번호를 입력해주세요.")
     private String storeNumber;
 
     //    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]{8,16}$",
