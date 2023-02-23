f9470ccad346844ab795630b98d6969dcd648d6c
HaDH <gusdk123123@naver.com>
2023-02-23 23:14:14 +0900

    결제 수단 페이지

--- src/main/resources/static/js/paymentOption.js
+++ src/main/resources/static/js/paymentOption.js
@@ -4,10 +4,10 @@ const cash = document.querySelector(".cash");
 
 // 카드 결제
 creditCard.onclick = () => {
-
+    location.href = "";
 }
 
 // 현금 결제
 cash.onclick = () => {
-
+    location.href = "";
 }
