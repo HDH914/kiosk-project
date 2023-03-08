d2c3ef44cc833a9186a0425b4478d58b2e1dc111
HaDH <gusdk123123@naver.com>
2023-03-08 19:20:47 +0900

    관리자 페이지 페이징 처리 완료

--- src/main/java/com/HDH/kiosk/service/menu/MenuServiceImpl.java
+++ src/main/java/com/HDH/kiosk/service/menu/MenuServiceImpl.java
@@ -57,24 +57,6 @@ public class MenuServiceImpl implements MenuService {
     }
 
     // 메뉴 리스트 불러오기
-//    @Override
-//    public List<MenuListRespDto> loadMenuList(int page, String category, String searchValue) throws Exception {
-//        Map<String, Object> paramsMap = new HashMap<String, Object>();
-//        paramsMap.put("index", (page - 1) * 10);
-//        paramsMap.put("limit", ((page - 1) * 10) + 10);
-//        paramsMap.put("category", category);
-//        paramsMap.put("searchValue", searchValue);
-//
-//        log.info("index: " + page);
-//        log.info("paramsMap: " + paramsMap);
-//        List<MenuListRespDto> list = new ArrayList<MenuListRespDto>();
-//        menuRepository.loadMenuList(paramsMap).forEach(MenuList -> {
-//            list.add(MenuList.toLoadMenu());
-//        });
-//        log.info("메뉴 리스트: " + list);
-//        return list;
-//    }
-
     @Override
     public List<MenuListRespDto> loadMenuList(int startIndex, String category, String searchValue) throws Exception {
         Map<String, Object> paramsMap = new HashMap<String, Object>();
@@ -84,6 +66,7 @@ public class MenuServiceImpl implements MenuService {
         paramsMap.put("searchValue", searchValue);
 
         log.info("paramsMap: " + paramsMap);
+
         List<MenuListRespDto> list = new ArrayList<MenuListRespDto>();
         menuRepository.loadMenuList(paramsMap).forEach(MenuList -> {
             list.add(MenuList.toLoadMenu());
