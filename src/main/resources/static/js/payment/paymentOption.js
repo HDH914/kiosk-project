43fe86ea48feaaceb04f584afcd343b06b0ffe2a
HaDH <gusdk123123@naver.com>
2023-03-11 16:33:50 +0900

    결제 서비스 제작중

--- src/main/resources/static/js/payment/paymentOption.js
+++ src/main/resources/static/js/payment/paymentOption.js
@@ -4,10 +4,11 @@ const cash = document.querySelector(".cash");
 
 // 카드 결제
 creditCard.onclick = () => {
-    location.href = "";
+    location.href = "/payment/option/card";
+
 }
 
 // 현금 결제
 cash.onclick = () => {
-    location.href = "";
+    location.href = "/payment/option/cash";
 }
